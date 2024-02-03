2142ef3b50bbfa8466588b34d49160943b47cfd7
polar-solvent <150511665+polar-solvent@users.noreply.github.com>
2024-02-03 18:33:26 +0900

    show.pyで流れる表示の機能を作成　経過時間も見られる

--- src/show.py
+++ src/show.py
@@ -1,13 +1,35 @@
 import os
+import re
+import cv2
+import time
 
 dir_path = "assets/dest"
 def is_frame(f):
-    if f[0:6] == 'frame_' and f[-4:] == '.bmp':
-        return True
-    else:
-        return False
+    return re.match(r"frame_\d+\.bmp", f)
 
 frames_name = [
     f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f)) and is_frame(f)
 ]
-print(frames_name)
+
+def sort_frame(f):
+    m = re.match(r"frame_(\d+)\.bmp", f)
+    return int(m.group(1))
+
+frames_name_sorted = sorted(frames_name,key=sort_frame)
+
+cv2.namedWindow("image")
+
+frames = [cv2.imread(f"./assets/dest/{f}") for f in frames_name_sorted]
+    
+st = time.perf_counter()
+
+for f in frames:
+    cv2.imshow("image", f)
+
+    cv2.waitKey(10)
+
+ed = time.perf_counter()
+
+cv2.destroyAllWindows()
+
+print(ed - st)
