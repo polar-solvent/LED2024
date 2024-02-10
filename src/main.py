6358ad36022b5c37889273618efe9d52e7d475b4
polar-solvent <150511665+polar-solvent@users.noreply.github.com>
2024-02-10 17:47:39 +0900

    main.pyの表示機能を削除

--- src/main.py
+++ src/main.py
@@ -7,8 +7,4 @@ h,w,c = img.shape
 img = np.hstack([np.zeros((h,SIZE,3),dtype=np.uint8),img,np.zeros((h,SIZE,3),dtype=np.uint8)])
 for i in range(w+SIZE):
     currentframe=img[:,i:i+SIZE,:]
-    cv2.imshow("image",currentframe)
-    
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
     cv2.imwrite(f"./assets/dest/frame_{i}.bmp",currentframe)
